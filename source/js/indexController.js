a0beebf75bb492ee72c60e30e1de43b3ab384a94
asasaki <asasaki@esapiens.com.br>
2018-08-09 12:49:23 -0300

    Criar chuva

--- source/js/indexController.js
+++ source/js/indexController.js
@@ -2,3 +2,10 @@ window.onload = function teste(){
     alert('dsffdsdsfsfd');
     console.log('dsasdadas');
 }
+
+$(document).ready(function() {
+    var n = 10;
+    for (var i=0; i < n; i++) {
+        $("#rain").append(`<div class="rain-drop"></div>`);
+    }
+});
