c09288ceb3e50d7ad6414070633896292b9a7db8
Alessandra Sasaki <ale.sasaki@hotmail.com>
2019-02-06 23:25:41 -0200

    criar task unificada para compilar todas as tarefas de uma vez

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+const babel = require('gulp-babel');
 var postcss = require('gulp-postcss');
-
-gulp.task('default', ['sass', 'sass:watch']);
+var runSequence = require('run-sequence');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('sass', function () {
     return gulp.src('./source/sass/style.sass')
@@ -13,17 +15,38 @@ gulp.task('sass', function () {
   });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./source/sass/style.sass', ['sass']);
+  return gulp.watch('./source/sass/style.sass', ['sass']);
 });
 
 gulp.task('autoprefixer', function () {
-  var postcss      = require('gulp-postcss');
-  var sourcemaps   = require('gulp-sourcemaps');
-  var autoprefixer = require('autoprefixer');
 
   return gulp.src('./source/sass/build/*.css')
       .pipe(sourcemaps.init())
       .pipe(postcss([ autoprefixer() ]))
       .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest('./source/sass/build'));
+});
+
+gulp.task('autoprefixer:watch', function () {
+  return gulp.watch('./source/sass/build/*.css', ['autoprefixer']);
+  callback();
+});
+ 
+gulp.task('babel', () => {
+  return gulp.src('./source/js/indexController.js')
+             .pipe(babel({
+                presets: ['@babel/env']
+              }))
+              .pipe(gulp.dest('source/js/build'))
+} );
+
+gulp.task('babel:watch', function () {
+  return gulp.watch('./source/js/indexController.js', ['babel']);
+});
+
+gulp.task('compile', function(callback) {
+  runSequence(['sass', 'sass:watch'],
+              ['babel', 'babel:watch'],
+              ['autoprefixer', 'autoprefixer:watch'],
+              callback);
 });
